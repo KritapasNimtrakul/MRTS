dbf0e4b743e8eb7a695255ab6ed19f21919159d3
KritapasNimtrakul <kxn9945@rit.edu>
2018-10-10 10:49:33 -0400

    update sprite (combat not working rn) need bugfixes

--- js/game.js
+++ js/game.js
@@ -1,12 +1,24 @@
 var Game = {};
-var testKey,enemyUnit,enemybase,unit,lane,spawnLane1Key,spawnLane2Key, base,enemyBase, overlay,canvas,buttonSpawn;
-var removeElement, allyGroup, enemyGroup, laneGroup, baseGroup;
+var testKey,player2base,lane,spawnLane1Key,spawnLane2Key, player1base, overlay,canvas,buttonSpawn;
+var removeElement, player1Group, player2Group, laneGroup, baseGroup;
 var playerSprite;
+let player1unit,player2unit;
+var player1CollisionGroup,player2CollisionGroup;
+var changeTimer;
 
 var combatMovement ={
     move:0,
     attack:1,
-    
+}
+
+var combat = {
+    waitTime:0.5,
+    attackDmg:1,
+    health:1,
+    range:1,
+    speed:1,
+    cost: ['b'],
+    decision:combatMovement.move,
 }
 
 Game.init = function(){
@@ -15,8 +27,8 @@ Game.init = function(){
 };
 
 Game.preload = function() {
-    game.load.image('base','assets/sprites/base.png');
-    game.load.image('enemybase','assets/sprites/base.png');
+    game.load.image('player1base','assets/sprites/base.png');
+    game.load.image('player2base','assets/sprites/base.png');
     game.load.image('lane','assets/sprites/lane.png');
     
     game.load.image('butter.png','assets/sprites/butter.png');
@@ -43,24 +55,36 @@ Game.create = function(){
     spawnLane3Key = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
     
     removeElement = game.add.group();
-    allyGroup = game.add.group();
-    enemyGroup = game.add.group();
+    player1Group = game.add.group();
+    player2group = game.add.group();
     laneGroup = game.add.group();
     baseGroup = game.add.group();
     
-    base = baseGroup.create(187, 375, 'base');
-
+    player1base = baseGroup.create(187, 375, 'player1base');
+    player2base = baseGroup.create(window.innerWidth-187, 375, 'player2base');
     
     testKey.onDown.add(Client.sendTest, this);
     spawnLane1Key.onDown.add(Client.spawnLane1, this);
     spawnLane2Key.onDown.add(Client.spawnLane2, this);
     spawnLane3Key.onDown.add(Client.spawnLane3, this);
     
-    base.inputEnabled = true;
-    base.events.onInputDown.add(listener, this);
+    player1base.inputEnabled = true;
+    player1base.events.onInputDown.add(listener, this);
   
-    game.physics.p2.enable(base);
-    base.body.static = true;
+    game.physics.p2.enable( [ player1base, player2base ]);
+    console.dir(player1base.body);
+    player1base.body.static = true;
+    player2base.body.static = true;
+    
+    player1CollisionGroup = game.physics.p2.createCollisionGroup();
+    player2CollisionGroup = game.physics.p2.createCollisionGroup();
+    
+    player1base.body.setCollisionGroup(player1CollisionGroup);
+    player2base.body.setCollisionGroup(player2CollisionGroup);
+    
+    player1base.body.collides([player1CollisionGroup, player2CollisionGroup]);
+    player2base.body.collides([player1CollisionGroup, player2CollisionGroup]);
+    
     
     overlay = document.querySelector('.b1');
     overlay.addEventListener('click',function(e){
@@ -99,35 +123,85 @@ function listener () {
 }
 
 Game.addNewPlayer = function(id){
-    enemybase = baseGroup.create(window.innerWidth-300, 0, 'enemybase');
+
     lane = laneGroup.create(10, 20, 'lane');
-    lane.scale.setTo(1, 0.8);
-    this.world.bringToTop(allyGroup);
-    this.world.bringToTop(enemyGroup);
+    lane.scale.setTo(0.7, 0.8);
+    this.world.bringToTop(player1Group);
+    this.world.bringToTop(player2group);
     this.world.bringToTop(baseGroup);
-    game.physics.add(enemyBase);
-    enemyBase.body.static = true;    
+
 };
 
 Game.addNewUnit = function(playerNum,x,y){
     if(playerNum == 0){
-        unit = allyGroup.create(x, y, playerSprite[0]);
-        unit.scale.setTo(0.25, 0.25);
-        game.physics.p2.enable(unit);
-        unit.body.data.gravityScale = 0.0;
-        unit.body.data.fixedY = true;  
-        unit.body.velocity.x = 100;
+        player1unit = player1Group.create(x, y, playerSprite[0]);
+        switch(playerSprite[0]){
+            case "butter.png": 
+                player1unit.combat = {...combat, health:2,speed:6 };
+                break;
+            case "flour.png": 
+                player1unit.combat = {...combat, attack:3,speed:2 };
+                break;
+            case "Sugar.png": 
+                player1unit.combat = {...combat, attack:2,speed:8 };
+                break;
+            default:
+                player1unit.combat = {...combat};
+                break;
+        }
+        
+        player1unit.scale.setTo(0.25, 0.25);
+        game.physics.p2.enable(player1unit);
+        player1unit.enableBody = true;
+        player1unit.physicsBodyType = Phaser.Physics.P2JS;
+        player1unit.body.data.gravityScale = 0.0;
+        player1unit.body.data.fixedY = true;
+        player1unit.body.data.fixedX = true;
+        player1unit.body.fixedRotation = true;
+        player1unit.body.velocity.x = 10*player1unit.combat.speed;
+        player1unit.body.velocity.y = 0;
+        player1unit.body.setCollisionGroup(player1CollisionGroup);
+        player1unit.body.collides([player1CollisionGroup, player2CollisionGroup]);
+        player1unit.body.collides(player2CollisionGroup, hitUnit, this);
+        console.dir(player1unit);
         
     }
     else{
-        enemyUnit = enemyGroup.create(window.innerWidth-x, y, playerSprite[1]);
-        enemyUnit.scale.setTo(0.25, 0.25);
-        game.physics.p2.enable(enemyUnit);
-        enemyUnit.body.data.gravityScale = 0.0;
-        enemyUnit.body.data.fixedY = true;  
-        enemyUnit.body.velocity.x = -100;
+        player2unit = player2group.create(window.innerWidth-x, y, playerSprite[1]);
+        switch(playerSprite[1]){
+            case "butter.png": 
+                player2unit.combat = {...combat, health:2,speed:6 };
+                break;
+            case "flour.png": 
+                player2unit.combat = {...combat, attack:3,speed:2 };
+                break;
+            case "Sugar.png": 
+                player2unit.combat = {...combat, attack:2,speed:8 };
+                break;
+            default:
+                player2unit.combat = {...combat};
+                break;
+            
+        }
+        console.dir(player2unit);
+        player2unit.scale.setTo(0.25, 0.25);
+        game.physics.p2.enable(player2unit);
+        player2unit.enableBody = true;
+        player2unit.physicsBodyType = Phaser.Physics.P2JS;
+        player2unit.body.data.gravityScale = 0.0;
+        player2unit.body.data.fixedY = true;  
+        player2unit.body.data.fixedX = true; 
+        player2unit.body.fixedRotation = true;
+        player2unit.body.velocity.x = -10*player2unit.combat.speed;
+        player2unit.body.setCollisionGroup(player2CollisionGroup);
+        player2unit.body.collides([player1CollisionGroup, player2CollisionGroup]);
+        player2unit.body.collides(player1CollisionGroup, hitUnit, this);
+        console.dir(player2unit);
+
     }
 
+            console.dir(player1CollisionGroup);
+            console.dir(player2CollisionGroup);
 };
 
 Game.ChangeUnit = function(spriteName,playerNum){
@@ -141,22 +215,6 @@ Game.removePlayer = function(id){
 };
 
 Game.update = function(){
-    for(var i= 0;i<allyGroup.length;i++){
-        //console.log(allyGroup.children[i]);
-        if(allyGroup.children[i].key == 'base'){
-            
-        }else{
-            allyGroup.children[i].x += 1;
-        }
-    }
-    for(var i= 0;i<enemyGroup.length;i++){
-        //console.log(allyGroup.children[i]);
-        if(enemyGroup.children[i].key == 'enemybase'){
-            
-        }else{
-            enemyGroup.children[i].x -= 1;
-        }
-    }
 }
 
 /*Game.dropHandler = function() {
@@ -170,3 +228,25 @@ Game.update = function(){
 
 
 }*/
+
+function hitUnit(body1, body2) {
+    console.log("hit");
+    
+    body1.combat.decision = combatMovement.attack;
+    if(body1.combat.decision == combatMovement.attack){
+        body1.body.velocity.x = 0;
+        game.time.events.loop(1000*body1.combat.waitTime, this.damageCalculation(body1,body2), this);
+    }
+
+
+}
+
+function damageCalculation(body1,body2) {
+    body2.combat.health -= body1.combat.attackDmg;
+    console.log("curr enemy health: " + body2.combat.health);
+    if(body2.combat.health <= 0){
+        body2.kill();
+        body1.combat.decision = combatMovement.move;
+        body1.body.velocity.x = 10*body1.combat.speed;
+    }
+}
