8826a62ea2dbd20074870b37ec1077af6e7704a1
Kritapas Nimtrakul (RIT Student) <kxn9945@ad.rit.edu>
2018-10-18 21:50:29 -0400

    b1 b2

--- js/main.js
+++ js/main.js
@@ -12,6 +12,8 @@ var config = {
 */
 var list1 = ["butter.png","flour.png","Milk.png","Sugar.png"];
 var list2 = ["cold.png","hot.png","oil.png","water.png"];
+var list3 = ["cracker","bread","lavacake","cupcake"];
+var list4 = ["Monstrosity","IceWater","MilkTea","buttermilk"];
 var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, document.getElementById('game'));
 var ui = document.createElement('div');
 ui.classList.add("js-overlay");
@@ -27,7 +29,7 @@ button1.appendChild(t1);
 buttonOverlay.appendChild(button1);
 
 var button2 = document.createElement('button');
-button2.classList.add("b1");
+button2.classList.add("b2");
 var t2 = document.createTextNode("S");
 button2.appendChild(t2);
 buttonOverlay.appendChild(button2);
@@ -68,6 +70,53 @@ InventoryOverlay.appendChild(InventorySlot);
 }
 
 ui.appendChild(InventoryOverlay);
+
+
+
+
+var SpecialOverlay = document.createElement('div');
+SpecialOverlay.classList.add("special-overlay");
+
+for(var i=0;i<4;i++){
+    
+var SpecialSlot = document.createElement('div');
+SpecialSlot.classList.add("special-slot");
+
+var slot1 = document.createElement('button');
+slot1.classList.add("slot");
+slot1.value = list3[i];
+
+var img1 = document.createElement('img');
+img1.classList.add("img1");
+img1.src = "../assets/sprites/"+list3[i];
+slot1.appendChild(img1);
+
+var slot2 = document.createElement('button');
+slot2.classList.add("slot");
+slot2.value = list4[i];
+
+var img2 = document.createElement('img');
+img2.classList.add("img1");
+img2.src = "../assets/sprites/"+list4[i];
+slot2.appendChild(img2);
+
+SpecialSlot.appendChild(slot1);
+SpecialSlot.appendChild(slot2);
+
+
+SpecialOverlay.appendChild(SpecialSlot);
+}
+
+ui.appendChild(SpecialOverlay);
+
+
+
+
+
+
+
+
+
 
 document.getElementById('game').appendChild(ui);
 game.state.add('Game',Game);
